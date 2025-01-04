65709cd6c09113e9199153e0abbba2ab1290e4af
hendisantika <hendisantika@yahoo.co.id>
2025-01-05 05:58:33 +0700

    Add JsonUtil🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/graylog/util/JsonUtil.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.graylog.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-graylog
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/01/25
+ * Time: 05.57
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+public class JsonUtil {
+
+    private static final ObjectMapper jsonMapper = new ObjectMapper();
+
+}
